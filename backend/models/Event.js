7ec8cdaebef2e5ea08ee2a595844d1fbfc1d0025
Samisb2002 <150432679+Samisb2002@users.noreply.github.com>
2025-01-08 23:42:00 +0100

    Add files via upload

---
+++ backend/models/Event.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const EventSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Event', EventSchema);
