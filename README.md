91185eaf72d4b615f4a678c71b0fbdad355c3b17
WalidHASNAOUI <walidhasnaouidilaw@gmail.com>
2025-01-08 21:29:06 +0100

    Clear main branch

--- README.md
+++
@@ -1,1 +0,0 @@
-# SportHUB-APP
--- backend/src/middlewares/error.middleware.js
+++
@@ -1,7 +0,0 @@
-// A global error handler to centralize error responses:
-
-export const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(err.status || 500).json({ message: err.message || "Internal Server Error" });
-  };
-  
