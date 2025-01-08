f7671df919743a77790cd60d956d31bed4b44104
Samisb2002 <150432679+Samisb2002@users.noreply.github.com>
2025-01-08 23:48:21 +0100

    Add files via upload

---
+++ backend/models/Post.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment',
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Post', PostSchema);
